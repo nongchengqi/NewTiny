6544341c6597c3a27e0115c611a9efdb355a3882
nongchengqi <nongcq@flatincbr.com>
2024-01-22 14:58:46 +0800

    1.add example

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,5 +43,6 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("com.github.jitpack:android-example:1.0.1")
     implementation(project(":tiny"))
 }
